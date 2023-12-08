37bf9f41bd07c424567f3f180ac2d1331774bd65
KEA55555 <isabellamail@yandex.ru>
2023-12-08 13:14:40 +0300

    My commit Diplom

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,20 +1,13 @@
 import configuration
 import requests
-import data
 
 
 # Создание нового заказа
 def post_new_order(new_order_body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
                          json=new_order_body)
-response = post_new_order(data.new_order_body)
-print(response.status_code)
-print(response.json())
 
 
 # Получение информации по новому заказу
 def get_new_order(track):
     return requests.get(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, params={"t": track})
-response = get_new_order(data.new_order_body)
-print(response.status_code)
-print(response.json())
